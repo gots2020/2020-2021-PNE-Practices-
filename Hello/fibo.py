442574353ac2813f115efade38eca03a287b7027
Guillermo Ots <g.ots.2020@alumnos.urjc.es>
2021-02-17 11:18:42 +0100

    added fibo.py to hello folder, and solved excercise 1.

--- Hello/fibo.py
+++ Hello/fibo.py
@@ -1,11 +1,14 @@
 
 def fibon(n):
+    new_list = [0, 1]
     x = 0
     y = 1
-    for i in range(1, n):
+    for i in range(1, n - 1):
         sum = x + y
         x = y
         y = sum
-    return sum
+        new_list.append(sum)
+    return new_list
 
-print("The sum of the first 10 fibonacci numbers is : " , fibon(10))
+print("The first 11 fibonacci numbers are : " , fibon(11))
+print("The first 15 fibonacci numbers are : " , fibon(15))

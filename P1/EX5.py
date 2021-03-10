787268e338770b346d76ddd2ef57cc7b19f6ef71
Guillermo Ots <g.ots.2020@alumnos.urjc.es>
2021-03-10 17:52:27 +0100

    all the exercises of P1

--- P1/EX5.py
+++ P1/EX5.py
@@ -3,12 +3,9 @@ from Seq1 import Seq
 s1 = Seq()
 s2 = Seq("ACTGTCCTG")
 s3 = Seq("Incorrect sequence")
+seq_list = [s1, s2, s3]
 
-print(f"\nSequence {str(0)}: (Length: {str(s1.len())}) {str(s1)}")
-Seq.print_count_bases(s1.count_bases())
+for i in range(1,len(seq_list) + 1):
+    print(f"\nSequence {str(i)}: (Length: {str(seq_list[i - 1].len())}) {str(seq_list[i - 1])}")
+    Seq.print_count_bases(seq_list[i - 1].count_bases())
 
-print(f"Sequence {str(1)}: (Length: {str(s2.len())}) {str(s2)}")
-Seq.print_count_bases(s2.count_bases())
-
-print(f"Sequence {str(2)}: (Length: {str(s3.len())}) {str(s3)}")
-Seq.print_count_bases(s3.count_bases())

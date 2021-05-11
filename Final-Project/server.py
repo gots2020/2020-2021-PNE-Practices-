38212b8fc2554f96fad361db6e0e35628556e7e8
Guillermo Ots <g.ots.2020@alumnos.urjc.es>
2021-05-11 17:01:42 +0200

    final project

---
+++ Final-Project/server.py
@@ -0,0 +1,100 @@
+import http.server
+import http.client
+import json
+import socketserver
+import termcolor
+import jinja2
+import pathlib
+from urllib.parse import urlparse, parse_qs
+
+
+PORT = 8080
+
+def read_template_html_file(filename):
+    content = jinja2.Template(pathlib.Path(filename).read_text())
+    return content
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+        termcolor.cprint(self.path, 'blue')
+
+        o = urlparse(self.path)
+        path_name = o.path
+        arguments = parse_qs(o.query)
+
+        print("Resource requested: ", path_name)
+        print("Parameters:", arguments)
+
+        SERVER = "rest.ensembl.org"
+        PARAMETERS = "?content-type=application/json"
+        connection = http.client.HTTPConnection(SERVER)
+
+        context = {}
+        if path_name == "/":
+            contents = read_template_html_file("./HTML/index.html").render()
+        elif path_name == "/listSpecies":
+            try:
+                ENDPOINT = "/info/species"
+                connection.request("GET", ENDPOINT + PARAMETERS)
+                response = connection.getresponse()
+                response_dict = json.loads(response.read().decode())
+                count = len(response_dict["species"])
+                new_list = []
+                for i in range(0, int(arguments["limit"][0])):
+                    new_list.append(response_dict["species"][i]["common_name"])
+                context = {"Total_seq": count,
+                           "limit": arguments["limit"][0],
+                           "names_seq": new_list}
+                contents = read_template_html_file("./HTML/listSpecies.html").render(context=context)
+            except ValueError:
+                contents = read_template_html_file("./HTML/error.html").render()
+        else:
+            contents = read_template_html_file("./HTML/error.html").render()
+
+        # Generating the response message
+        self.send_response(200)  # -- Status line: OK!
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(contents.encode()))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(contents.encode())
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()

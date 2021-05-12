172c8081c7b4f2cd9824458cbfea38d936d18803
Guillermo Ots <g.ots.2020@alumnos.urjc.es>
2021-05-12 18:54:43 +0200

    final project

--- Final-Project/server.py
+++ Final-Project/server.py
@@ -7,6 +7,19 @@ import jinja2
 import pathlib
 from urllib.parse import urlparse, parse_qs
 
+genes_dict = {
+    "FRAT1": "ENSG00000165879",
+    "ADA": "ENSG00000196839",
+    "FXN": "ENSG00000165060",
+    "RNU6_269P": "ENSG00000212379",
+    "MIR633": "ENSG00000207552",
+    "TTTY4C": "ENSG00000228296",
+    "RBMY2YP": "ENSG00000227633",
+    "FGFR3": "ENSG00000068078",
+    "KDR": "ENSG00000128052",
+    "ANK2": "ENSG00000145362"
+}
+
 
 PORT = 8080
 

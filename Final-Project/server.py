5a119db2302448c5d6067cb3bb8cee21acf0d567
Guillermo Ots <g.ots.2020@alumnos.urjc.es>
2021-05-30 14:06:30 +0200

    final project

--- Final-Project/server.py
+++ Final-Project/server.py
@@ -6,6 +6,7 @@ import termcolor
 import jinja2
 import pathlib
 from urllib.parse import urlparse, parse_qs
+from P7 import Seq1
 
 genes_dict = {
     "FRAT1": "ENSG00000165879",
@@ -19,7 +20,6 @@ genes_dict = {
     "KDR": "ENSG00000128052",
     "ANK2": "ENSG00000145362"
 }
-
 
 PORT = 8080
 
@@ -103,7 +103,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 context = {"Chromosome_names": response_dict["karyotype"]}
                 contents = read_template_html_file("./HTML/karyotype.html").render(context=context)
             else:
-                contents = read_template_html_file("./HTML/error_specie.html").render()
+                contents = read_template_html_file("HTML/error_specie.html").render()
         elif path_name == "/chromosomeLength":
             try:
                 specie = arguments["specie"][0].replace(" ", "_")
@@ -118,9 +118,57 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                             context = {"Chromosome_length": d["length"]}
                     contents = read_template_html_file("./HTML/chromosomeLength.html").render(context=context)
                 else:
-                    contents = read_template_html_file("./HTML/error_specie.html").render()
+                    contents = read_template_html_file("HTML/error_specie.html").render()
             except KeyError:
-                contents = read_template_html_file("./HTML/error_specie.html").render()
+                contents = read_template_html_file("HTML/error_specie.html").render()
+        elif path_name == "/geneSeq":
+            try:
+                gene = arguments["gene"][0].replace(" ", "_")
+                id = genes_dict[gene]
+                ENDPOINT = "/sequence/id/"
+                connection.request("GET", ENDPOINT + id + PARAMETERS)
+                response = connection.getresponse()
+                if response.status == 200:
+                    response_dict = json.loads(response.read().decode())
+                    context = {"sequence": response_dict["seq"]}
+                    contents = read_template_html_file("./HTML/gene_seq.html").render(context=context)
+                else:
+                    contents = read_template_html_file("HTML/error_gene.html").render()
+            except KeyError:
+                contents = read_template_html_file("HTML/error_gene.html").render()
+        elif path_name == "/geneInfo":
+            try:
+                gene = arguments["gene"][0].replace(" ", "_")
+                id = genes_dict[gene]
+                ENDPOINT = "/sequence/id/"
+                connection.request("GET", ENDPOINT + id + PARAMETERS)
+                response = connection.getresponse()
+                if response.status == 200:
+                    response_dict = json.loads(response.read().decode())
+                    info_gene = response_dict["desc"]
+                    info_data_gene = info_gene.split(":")
+                    length_gene = int(info_data_gene[4]) - int(info_data_gene[3]) + 1
+                    context = {"gene_name": gene, "start": info_data_gene[3], "end": info_data_gene[4],
+                               "length": length_gene, "id": id, "chromosome_name": info_data_gene[2]}
+                    contents = read_template_html_file("./HTML/gene_info.html").render(context=context)
+            except KeyError:
+                contents = read_template_html_file("HTML/error_gene.html").render()
+        elif path_name == "/geneCalc":
+            try:
+                gene = arguments["gene"][0].replace(" ", "_")
+                id = genes_dict[gene]
+                ENDPOINT = "/sequence/id/"
+                connection.request("GET", ENDPOINT + id + PARAMETERS)
+                response = connection.getresponse()
+                if response.status == 200:
+                    response_dict = json.loads(response.read().decode())
+                    sequence = Seq1.Seq(response_dict["seq"])
+                    percentages = sequence.percentage_base_and_count()
+                    context = {"gene_name": gene, "total_length": sequence.len(), "A": percentages["A"], "C": percentages["C"],
+                               "G": percentages["G"], "T": percentages["T"]}
+                    contents = read_template_html_file("./HTML/gene_calc.html").render(context=context)
+            except KeyError:
+                contents = read_template_html_file("HTML/error_gene.html").render()
         else:
             contents = read_template_html_file("./HTML/error.html").render()
 

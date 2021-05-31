1adf96fe2991e3e6d53bef1fa9f65dc730dc4d5e
Guillermo Ots <g.ots.2020@alumnos.urjc.es>
2021-05-31 18:45:46 +0200

    final project

--- Final-Project/client.py
+++ Final-Project/client.py
@@ -26,16 +26,25 @@ SERVER = '127.0.0.1'
 print(f"\nConnecting to server: {SERVER}:{PORT}\n")
 
 conn = http.client.HTTPConnection(SERVER, PORT)
-try:
-    conn.request("GET", "/?json=1")
-except ConnectionRefusedError:
-    print("ERROR! Cannot connect to the Server")
-    exit()
-r1 = conn.getresponse()
-print(f"Response received!: {r1.status} {r1.reason}\n")
-data1 = r1.read().decode("utf-8")
-json_string = json.dumps(data1)
-json_dict = json.loads(json_string)
+list_endpoints = ["/?json=1", "/listSpecies?limit=4&json=1", "/listSpecies?limit=sdg&json=1",
+                  "/karyotype?specie=homo_sapiens?json=1", "/karyotype?specie=fs2r?json=1",
+                  "/chromosomeLength?specie=homo_sapiens&chromo=1?json=1", "/chromosomeLength?specie=homo_sapiens&chromo=56h6?json=1",
+                  "/chromosomeLength?specie=&chromo=?json=1", "/geneSeq?gene=FRAT1&json=1", "/geneSeq?gene=gsd3r4&json=1",
+                  "/geneInfo?gene=FRAT1&json=1", "/geneInfo?gene=dfs465&json=1", "/geneCalc?gene=FRAT1&json=1",
+                  "/geneCalc?gene=gaagg4&json=1"]
 
-print(f"CONTENT:\n {json_dict}")
+for endpoint in list_endpoints:
+    try:
+        print(f"Test for endpoint {endpoint}:\n")
+        conn.request("GET", endpoint)
+    except ConnectionRefusedError:
+        print("ERROR! Cannot connect to the Server")
+        exit()
+    r1 = conn.getresponse()
+    print(f"Response received!: {r1.status} {r1.reason}\n")
+    data1 = r1.read().decode("utf-8")
+    json_string = json.dumps(data1)
+    json_dict = json.loads(json_string)
+
+    print(f"CONTENT:\n {json_dict}\n")
 

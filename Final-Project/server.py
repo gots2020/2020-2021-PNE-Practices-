0cb695bb16dfcd278831abce87784b14841dae84
Guillermo Ots <g.ots.2020@alumnos.urjc.es>
2021-05-31 19:39:23 +0200

    final project

--- Final-Project/server.py
+++ Final-Project/server.py
@@ -27,19 +27,14 @@ def read_template_html_file(filename):
     content = jinja2.Template(pathlib.Path(filename).read_text())
     return content
 
-# -- This is for preventing the error: "Port already in use"
+
 socketserver.TCPServer.allow_reuse_address = True
 
 
-# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
-# It means that our class inheritates all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        """This method is called whenever the client invokes the GET method
-        in the HTTP protocol request"""
 
-        # Print the request line
         termcolor.cprint(self.requestline, 'green')
         termcolor.cprint(self.path, 'blue')
 
@@ -341,29 +336,26 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         else:
             contents = read_template_html_file("./HTML/error.html").render()
 
-        # Generating the response message
-        self.send_response(200)  # -- Status line: OK!
 
-        # Define the content-type header:
+        self.send_response(200)
+
+
         self.send_header('Content-Type', content_type)
         self.send_header('Content-Length', len(contents.encode()))
 
-        # The header is finished
+
         self.end_headers()
 
-        # Send the response message
+
         self.wfile.write(contents.encode())
 
         return
 
 
-# ------------------------
-# - Server MAIN program
-# ------------------------
-# -- Set the new handler
+
 Handler = TestHandler
 
-# -- Open the socket server
+
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
 
     print("Serving at PORT", PORT)
